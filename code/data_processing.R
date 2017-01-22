1c48860c24f781052186f6638d9cc94a51e175be
Matthias Lux <mlux@uber.com>
2017-01-22 15:26:02 -0800

    Corrected faulty path for data

--- code/data_processing.R
+++ code/data_processing.R
@@ -6,12 +6,12 @@ rm(list=ls())
 require('rugarch')
 
 # read data in: LIBOR
-libor <- read.csv('USD12MD156N.csv')
+libor <- read.csv('../data/USD12MD156N.csv')
 names(libor)[names(libor) == 'USD12MD156N'] <- 'libor12m'
 libor$libor12m <- as.numeric(levels(libor$libor12m)[libor$libor12m])
 
 # read data in: CMT
-cmt <- read.csv('DGS1.csv')
+cmt <- read.csv('../data/DGS1.csv')
 names(cmt)[names(cmt) == 'DGS1'] <- 'cmt_rate'
 cmt$cmt_rate <- as.numeric(levels(cmt$cmt_rate)[cmt$cmt_rate])
 
